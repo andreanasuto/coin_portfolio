528cfefe3faac0bc1b9e5dacbebb5231f34986a6
Andrea Nasuto <andrea.nasuto@gmail.com>
2017-11-24 20:16:58 +0000

    coin controller first steps as show, edit

--- app/controllers/coin_controller.rb
+++ app/controllers/coin_controller.rb
@@ -1,3 +1,13 @@
 class CoinController < ApplicationController
 
+  get '/coins/new' do
+  end
+
+  get '/coins/:id' do
+    erb :"coins/show"
+  end
+
+  get '/coins/:id/edit' do
+    erb :"coins/edit"
+  end
 end
